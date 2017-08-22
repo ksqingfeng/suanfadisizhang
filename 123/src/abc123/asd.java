e229e521fa0151b1c9704e1ac20db955fd0cbcce
ksqingfeng <906482164@qq.com>
2017-08-22 09:54:58 +0800

    123

---
+++ 123/src/abc123/asd.java
@@ -0,0 +1,22 @@
+package abc123;
+import java.util.*;
+
+public class asd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double y=1.0/0.0;
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int c=in.nextInt();
+		if(a==b&&b==c)
+			System.out.println("equal");
+		else
+			System.out.println("not equal");
+//		System.out.println("hello world");
+//		System.out.println(y);
+//		System.out.println(new Date());
+	}
+
+}
